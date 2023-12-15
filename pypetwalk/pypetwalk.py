b7fd65049a91c63e85c9ec1bd7ead338a2e7ef8a
p0l0 <1452110+p0l0@users.noreply.github.com>
2023-12-15 11:07:09 +0100

    More methods for integration

--- pypetwalk/pypetwalk.py
+++ pypetwalk/pypetwalk.py
@@ -61,9 +61,15 @@ class PyPetWALK:
 
     async def get_api_data(self):
         try:
-            modes = await self.api_client.get_modes()
-            states = await self.api_client.get_states()
-            return modes | states
+            modes = await self.get_modes()
+            states = await self.get_states()
+            response = modes | states
+
+            for key in response.keys():
+                if response[key] in API_STATE_MAPPING:
+                    response[key] = API_STATE_MAPPING[response[key]]
+
+            return response
         finally:
             await self.api_client.close()
 
@@ -103,7 +109,7 @@ class PyPetWALK:
     async def set_brightness_sensor(self, state: bool) -> bool:
         """Set new value for brightness sensor."""
         try:
-            return await self.__api_set_state(API_STATE_BRIGHTNESS_SENSOR, state)
+            return await self.set_state(API_STATE_BRIGHTNESS_SENSOR, state)
         finally:
             await self.api_client.close()
 
@@ -116,7 +122,7 @@ class PyPetWALK:
 
     async def set_motion_in(self, state: bool) -> bool:
         """Set new value for 'motion in' mode."""
-        return await self.__api_set_state(API_STATE_MOTION_IN, state)
+        return await self.set_state(API_STATE_MOTION_IN, state)
 
     async def get_motion_in(self) -> bool:
         """Get value for the 'motion in' mode."""
@@ -124,7 +130,7 @@ class PyPetWALK:
 
     async def set_motion_out(self, state: bool) -> bool:
         """Set new value for 'motion out' mode."""
-        return await self.__api_set_state(API_STATE_MOTION_OUT, state)
+        return await self.set_state(API_STATE_MOTION_OUT, state)
 
     async def get_motion_out(self) -> bool:
         """Get value for the 'motion out' mode."""
@@ -132,7 +138,7 @@ class PyPetWALK:
 
     async def set_rfid(self, state: bool) -> bool:
         """Set new value for rfid mode."""
-        return await self.__api_set_state(API_STATE_RFID, state)
+        return await self.set_state(API_STATE_RFID, state)
 
     async def get_rfid(self) -> bool:
         """Get value for the rfid mode."""
@@ -140,7 +146,7 @@ class PyPetWALK:
 
     async def set_time(self, state: bool) -> bool:
         """Set new value for time mode."""
-        return await self.__api_set_state(API_STATE_TIME, state)
+        return await self.set_state(API_STATE_TIME, state)
 
     async def get_time(self) -> bool:
         """Get value for the time mode."""
@@ -148,7 +154,7 @@ class PyPetWALK:
 
     async def set_door_state(self, state: bool) -> bool:
         """Open or closes petWALK door."""
-        return await self.__api_set_state(API_STATE_DOOR, state)
+        return await self.set_state(API_STATE_DOOR, state)
 
     async def get_door_state(self) -> bool:
         """Get the current door state."""
@@ -156,7 +162,7 @@ class PyPetWALK:
 
     async def set_system_state(self, state: bool) -> bool:
         """Turn petWALK on or off."""
-        return await self.__api_set_state(API_STATE_SYSTEM, state)
+        return await self.set_state(API_STATE_SYSTEM, state)
 
     async def get_system_state(self) -> bool:
         """Get current petWALK system state."""
@@ -183,14 +189,17 @@ class PyPetWALK:
 
         return result  # type: ignore[no-any-return]
 
-    async def __api_set_state(self, param: str, value: bool) -> bool:
+    async def set_state(self, param: str, value: bool) -> bool:
         """Call API method to set new value for requested mode/state."""
-        method = f"set_{API_METHOD_MAPPING[param].lower()}"
-        _LOGGER.debug(
-            "Calling API method %s for %s with value %r", method, param, value
-        )
-        await getattr(self.api_client, method)(param, value)
-        return True
+        try:
+            method = f"set_{API_METHOD_MAPPING[param].lower()}"
+            _LOGGER.debug(
+                "Calling API method %s for %s with value %r", method, param, value
+            )
+            await getattr(self.api_client, method)(param, value)
+            return True
+        finally:
+            await self.api_client.close()
 
     async def get_device_info(self) -> dict:
         """Get current device information."""
